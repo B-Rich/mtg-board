e9334447e59f70b64de9544e346265981771c1d5
Bruno Saibro Silveira <djbrunosilveira@gmail.com>
2015-11-23 13:33:56 -0200

    Sets up foundation and add tabs to board and history

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ import History from './src/components/history';
 let App = React.createClass({
   render: function() {
 		return (
-      <div>
-        <Players />
-        <Controls />
-        <History />
+      <div className={'row'}>
+        <ul className={'tabs'} data-tab role='tablist'>
+          <li className={'tabs-title is-active'}><a href="#board">Board</a></li>
+          <li className={'tabs-title'}><a href="#history">History</a></li>
+        </ul>
+        <div className={'tabs-content'}>
+          <div className={'tabs-panel is-active'} id='board'>
+            <Players />
+            <Controls />
+          </div>
+          <div className={'tabs-panel'} id='history'>
+            <History />
+          </div>
+        </div>
 		  </div>
     );
 	}
